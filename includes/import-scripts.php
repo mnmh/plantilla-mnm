6eddb02b37d3db395173a8d068ec690c67f76f06
Néstor Andrés Peña <nestor2509@gmail.com>
2019-07-16 21:34:04 -0500

    inicio repositorio

---
+++ includes/import-scripts.php
@@ -0,0 +1,9 @@
+add_action('init', 'header_scripts');
+
+function header_scripts()
+{
+    if ($GLOBALS['pagenow'] != 'wp-login.php' && !is_admin()) {
+        wp_register_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.0');
+        wp_enqueue_script('scripts');
+    }
+}
