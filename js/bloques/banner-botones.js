d960928070f68010598500d97a596924280e2dba
Néstor Andrés Peña <nestor2509@gmail.com>
2019-08-06 07:09:08 -0500

    arreglo banner con botones

--- js/bloques/banner-botones.js
+++ js/bloques/banner-botones.js
@@ -4,27 +4,29 @@
 
         $('.banner-botones').each(function () {
             var index = 0;
-            var total = $(this).find('.item_nav').length - 1;
+            var total = $(this).find('.item_nav').length;
 
-            $parent = $(this);
-            $parent.find('.item_nav:nth-child(' + (index + 1) + ')').addClass('active');
-            $parent.find('.img').css({
-                'background-image': 'url("' + $parent.find('.item_nav:nth-child(' + (index + 1) + ')').attr('data-img') + '")'
+            $parent_temporal = $(this);
+            $parent_temporal.find('.item_nav:nth-child(' + (index + 1) + ')').addClass('active');
+            $parent_temporal.find('.img').css({
+                'background-image': 'url("' + $parent_temporal.find('.item_nav:nth-child(' + (index + 1) + ')').attr('data-img') + '")'
             });
 
-            $(this).find('.inside_navigation .nav').on('click', function () {
+            $parent_temporal.find('.inside_navigation .nav').on('click', function () {
                 if ($(this).hasClass('next')) {
-                    if (index < total) index++;
+                    if (index < total - 1) index++;
                     else index = 0;
                 } else {
-                    if (index > 0) index--;
+                    if (index > 1) index--;
                     else index = total;
                 }
 
-                $parent.find('.item_nav.active').removeClass('active');
-                $parent.find('.item_nav:nth-child(' + (index + 1) + ')').addClass('active');
-                $parent.find('.img').css({
-                    'background-image': 'url("' + $parent.find('.item_nav:nth-child(' + (index + 1) + ')').attr('data-img') + '")'
+
+                $newItem = $parent_temporal.find('.item_nav:nth-child(' + (index + 1) + ')');
+                $parent_temporal.find('.item_nav.active').removeClass('active');
+                $newItem.addClass('active');
+                $parent_temporal.find('.img').css({
+                    'background-image': 'url("' + $newItem.attr('data-img') + '")'
                 });
             });
         });
