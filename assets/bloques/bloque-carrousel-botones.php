24b57bdeda6267804d32581ff6c932fab17fb815
Laura Junco <lc.junco10@uniandes.edu.co>
2019-09-02 10:45:46 -0500

    Centrar titulo de seccion y darle un ancho máximo. Estilos carrusel botones termnados :)

--- assets/bloques/bloque-carrousel-botones.php
+++ assets/bloques/bloque-carrousel-botones.php
@@ -12,7 +12,7 @@ $titulo = get_field('titulo');
 ?>
 
 <div class="botones-carrousel btm">
-    <h1 class="sec pad"><?php echo $titulo; ?></h1>
+    <h1 class="sec"><?php echo $titulo; ?></h1>
     <nav class="hori_scroll">
         <div class="prev"></div>
         <div class="next"></div>
