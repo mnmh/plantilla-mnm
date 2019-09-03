122609a6cc8814afcef074029e44ded26eea66fa
Laura Junco <lc.junco10@uniandes.edu.co>
2019-09-03 10:29:26 -0500

    Estilizar subtitulo de secciones y agregar subtitulo para bloque carrusel botones

--- assets/bloques/bloque-carrousel-botones.php
+++ assets/bloques/bloque-carrousel-botones.php
@@ -13,7 +13,7 @@ $titulo = get_field('titulo');
 
 <div class="botones-carrousel btm">
     <h1 class="sec"><?php echo $titulo; ?></h1>
-    <div class="subtitulo pad">Subtitulo</div>
+    <div class="subtitulo">Lorem ipsum dolor sit amet</div>
     <nav class="hori_scroll">
         <div class="prev"></div>
         <div class="next"></div>
@@ -27,6 +27,7 @@ $titulo = get_field('titulo');
             <li class="boton">
                 <div class="img" style="background-image:url('<?php echo $imagen['sizes']['large'] ?>')"></div>
                 <h2 class="name"><?php the_sub_field('nombre') ?></h2>
+                <p class="subtitulo-tarjetas"> Lorem ipsum dolor sit amet </p>
                 <a href="#" class="btn">Ver más</a>
             </li>
         <?php endwhile; endif; ?>
