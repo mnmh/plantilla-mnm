8ac2c04ffef5f3927fefe83285de5c62643eb722
Laura Junco <lc.junco10@uniandes.edu.co>
2019-09-04 12:52:42 -0500

    Tratar de cuadrar las tarjetas de banner voces

--- assets/bloques/bloque-banner-tarjetas-audio.php
+++ assets/bloques/bloque-banner-tarjetas-audio.php
@@ -19,14 +19,16 @@ $enlace = get_field('enlace');
                 <div class="contenedor-txt"> <div class="txt"></div> </div>
             </div>
 
-            <div class="tarjetas">
-                <?php if(have_rows('ejes')): while(have_rows('ejes')): the_row(); ?>
-                <?php $imagen = get_sub_field('imagen') ?>
-                <?php $thumbnail = get_sub_field('thumbnail') ?>
-                <div class="item" data-titulo="<?php echo get_sub_field('titulo')?>" data-des="<?php echo get_sub_field('descripcion')?>" data-imagen="background-image:url('<?php echo $imagen['sizes']['large'] ?>')"  style="background-image:url('<?php echo $thumbnail['sizes']['large'] ?>')">
-                    <div class="btn-play play"> Escuchar</div>
+            <div class="contenedor-tarjetas">
+                <div class="tarjetas">
+                    <?php if(have_rows('ejes')): while(have_rows('ejes')): the_row(); ?>
+                    <?php $imagen = get_sub_field('imagen') ?>
+                    <?php $thumbnail = get_sub_field('thumbnail') ?>
+                    <div class="item" data-titulo="<?php echo get_sub_field('titulo')?>" data-des="<?php echo get_sub_field('descripcion')?>" data-imagen="background-image:url('<?php echo $imagen['sizes']['large'] ?>')"  style="background-image:url('<?php echo $thumbnail['sizes']['large'] ?>')">
+                        <div class="btn-play play"> Escuchar</div>
+                    </div>
+                    <?php endwhile; endif; ?>
                 </div>
-                <?php endwhile; endif; ?>
             </div>
         </div>
 </div>
