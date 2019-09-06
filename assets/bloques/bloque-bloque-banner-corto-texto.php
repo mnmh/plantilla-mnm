991bd70d3d9dd57098d8330173cf6e6bfa72cc4f
Laura Junco <lc.junco10@uniandes.edu.co>
2019-09-06 11:06:36 -0500

    Bloque banner-corto-texto listo y responsive :)

--- assets/bloques/bloque-bloque-banner-corto-texto.php
+++ assets/bloques/bloque-bloque-banner-corto-texto.php
@@ -12,13 +12,10 @@ $imagen = get_field('imagen');
 
 ?>
 
-<div class="banner texto-imagen-corto  btm "> 
+<div class="banner texto-imagen-corto  btm" style="background-image:url('<?php echo $imagen['url']; ?>')"> 
     <div class="contenedor-banner">
         <div class="left">
             <h1 class="titulo"><?php echo $texto; ?></h1>
-        </div>
-        <div class="right">
-            <div class="img" style="background-image:url('<?php echo $imagen['url']; ?>')"></div>
         </div>
     </div>
 </div>
