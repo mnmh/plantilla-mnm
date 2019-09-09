7d10a5d6b7f2acf84abbdf72d79faf6a0658c794
Nestor Pena <nestor2509@gmail.com>
2019-09-09 18:47:46 -0500

    bloque equipo campos arreglados

--- includes/campos/bloque-equipo.php
+++ includes/campos/bloque-equipo.php
@@ -132,7 +132,7 @@ if( function_exists('acf_add_local_field_group') ):
                 array(
                     'param' => 'block',
                     'operator' => '==',
-                    'value' => 'all',
+                    'value' => 'bloque equipo',
                 ),
             ),
         ),
