859c3e73fc28cd0224b656836ba745a7c5c49147
Nestor Pena <nestor2509@gmail.com>
2019-09-10 14:59:34 -0500

    tipo eventos

--- assets/bloques/bloque-bloque-agenda.php
+++ assets/bloques/bloque-bloque-agenda.php
@@ -66,7 +66,8 @@ foreach($posts as $post):
     $participante = get_field('participante', $id);
 
     $tipo = get_field('tipo_ev', $id);
-    if(!$tipo) $tipo = '';
+    // if($tipo) $tipo = '';
+    print_r($tipo);
 
     $mes_temp = date('F',$time);
     $mes_temp = darMesEspanol($mes_temp);
