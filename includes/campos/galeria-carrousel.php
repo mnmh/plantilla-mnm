920a9eadbddd2569d3f88643ddcbff944d3f4980
Nestor Pena <nestor2509@gmail.com>
2019-09-09 19:42:07 -0500

    campos galeria carrousel

---
+++ includes/campos/galeria-carrousel.php
@@ -0,0 +1,56 @@
+<?php
+if( function_exists('acf_add_local_field_group') ):
+
+    acf_add_local_field_group(array(
+        'key' => 'group_5d670799972f2',
+        'title' => 'BLOQUE galeria carrousel',
+        'fields' => array(
+            array(
+                'key' => 'field_5d6707a0aaee0',
+                'label' => 'Galería',
+                'name' => 'galeria',
+                'type' => 'gallery',
+                'instructions' => '',
+                'required' => 0,
+                'conditional_logic' => 0,
+                'wrapper' => array(
+                    'width' => '',
+                    'class' => '',
+                    'id' => '',
+                ),
+                'return_format' => 'array',
+                'preview_size' => 'medium',
+                'insert' => 'append',
+                'library' => 'all',
+                'min' => '',
+                'max' => '',
+                'min_width' => '',
+                'min_height' => '',
+                'min_size' => '',
+                'max_width' => '',
+                'max_height' => '',
+                'max_size' => '',
+                'mime_types' => '',
+            ),
+        ),
+        'location' => array(
+            array(
+                array(
+                    'param' => 'block',
+                    'operator' => '==',
+                    'value' => 'acf/galeria-carrousel',
+                ),
+            ),
+        ),
+        'menu_order' => 0,
+        'position' => 'normal',
+        'style' => 'default',
+        'label_placement' => 'top',
+        'instruction_placement' => 'label',
+        'hide_on_screen' => '',
+        'active' => true,
+        'description' => '',
+    ));
+    
+    endif;
+?>
