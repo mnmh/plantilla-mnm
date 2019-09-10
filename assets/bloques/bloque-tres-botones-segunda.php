5b0e0423ee5e0a20b4496dcc8bd2cf05b5f708e7
Nestor Pena <nestor2509@gmail.com>
2019-09-10 16:13:36 -0500

    enlaces tres botones segunda version

--- assets/bloques/bloque-tres-botones-segunda.php
+++ assets/bloques/bloque-tres-botones-segunda.php
@@ -19,7 +19,7 @@ $id = 'bloque-' . $block['id'];
             <li class="boton">
                 <div class="img" style="background-image:url('<?php echo $imagen['sizes']['large'] ?>')"></div>
                 <div class="name">
-                    <a class="btn" href="#"><?php the_sub_field('nombre') ?> </a>
+                    <a class="btn" href="<?php echo get_sub_field('enlace') ?>"><?php the_sub_field('nombre') ?> </a>
                     <p><?php the_sub_field('descripcion') ?></p>
                 </div>
             </li>
