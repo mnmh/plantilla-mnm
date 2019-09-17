e35582c8e7bdb6a407544131df8a1e6d84450a69
Laura Junco <lc.junco10@uniandes.edu.co>
2019-09-17 11:31:37 -0500

    Agregar br a campo texto banner voces

--- includes/campos/banner-ejes.php
+++ includes/campos/banner-ejes.php
@@ -22,7 +22,7 @@ if( function_exists('acf_add_local_field_group') ):
                 'placeholder' => '',
                 'maxlength' => '',
                 'rows' => '',
-                'new_lines' => '',
+                'new_lines' => 'br',
             ),
             array(
                 'key' => 'field_5d66f45f6b590',
