48f8e67020dc28e5b9857e99adca6a78768fb8ea
Laura Junco <lc.junco10@uniandes.edu.co>
2019-09-26 12:43:46 -0500

    Agregar tarjetas a bloque right

--- assets/bloques/bloque-bloque-personajes.php
+++ assets/bloques/bloque-bloque-personajes.php
@@ -17,36 +17,38 @@ $enlace = get_field('enlace');
 
         <div class="left">
 
-        <div class="textos">
-                <h1> 
-                    Grandes conversaciones
-                </h1>
-                <p> 
-                    A lo largo de sus vidas, Víctor Gaviria, Felipe Aljure y Mariana Arruti se han preocupado por dejar en la memoria historias de la gente anónima. 
-                </p>
-        </div> 
-    
+            <div class="textos">
+                    <h1> 
+                        Grandes conversaciones
+                    </h1>
+                    <p> 
+                        A lo largo de sus vidas, Víctor Gaviria, Felipe Aljure y Mariana Arruti se han preocupado por dejar en la memoria historias de la gente anónima. 
+                    </p>
+            </div> 
+        
 
-        <div class="reproductor">
-            <div class="slider">
-                <div class="current"></div>
-                <div class="total"></div>
+            <div class="reproductor">
+                <div class="slider">
+                    <div class="current"></div>
+                    <div class="total"></div>
+                </div>
+                <div class="time">
+                    00:00 / 00:00
+                </div>
             </div>
-            <div class="time">
-                00:00 / 00:00
+
+        </div>
+ 
+        <div class="tarjetas right">
+            <?php if(have_rows('ejes')): while(have_rows('ejes')): the_row(); ?>
+            <?php $imagen = get_sub_field('imagen') ?>
+            <?php $thumbnail = get_sub_field('thumbnail') ?>
+            <?php $audio = get_sub_field('audio') ?>
+            <div class="item" data-titulo="<?php echo get_sub_field('titulo')?>" data-des="<?php echo get_sub_field('descripcion')?>" data-imagen="background-image:url('<?php echo $imagen['sizes']['large'] ?>')"  style="background-image:url('<?php echo $thumbnail['sizes']['large'] ?>')">
+                <div class="btn-play play" data-audio="<?php echo $audio['url'] ?>"> Escuchar</div>
             </div>
+            <?php endwhile; endif; ?>
         </div>
 
     </div>
-
-    <div class="tarjetas">
-        <?php if(have_rows('ejes')): while(have_rows('ejes')): the_row(); ?>
-        <?php $imagen = get_sub_field('imagen') ?>
-        <?php $thumbnail = get_sub_field('thumbnail') ?>
-        <?php $audio = get_sub_field('audio') ?>
-        <div class="item" data-titulo="<?php echo get_sub_field('titulo')?>" data-des="<?php echo get_sub_field('descripcion')?>" data-imagen="background-image:url('<?php echo $imagen['sizes']['large'] ?>')"  style="background-image:url('<?php echo $thumbnail['sizes']['large'] ?>')">
-            <div class="btn-play play" data-audio="<?php echo $audio['url'] ?>"> Escuchar</div>
-        </div>
-        <?php endwhile; endif; ?>
-    </div> 
 </div>
