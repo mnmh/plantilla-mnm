093e686d79a9e7e01051994e7513865a10986e0f
Nestor Pena <nestor2509@gmail.com>
2019-09-30 10:28:29 -0500

    arreglos del js de la lista de tarjetas

--- js/bloques/lista-tarjetas.js
+++ js/bloques/lista-tarjetas.js
@@ -87,7 +87,7 @@
         });
 
 
-        $('.tarjetas .item.video').on('click', function () {
+        $('.tarjetas .item.tipo_video').on('click', function () {
             var video = $(this).attr('data-video');
             var url = 'https://www.youtube.com/embed/' + video + '?autoplay=1&controls=0&rel=0&showinfo=0&color=white';
             $('body').append('<div class="visor_video"><iframe></iframe></div>');
