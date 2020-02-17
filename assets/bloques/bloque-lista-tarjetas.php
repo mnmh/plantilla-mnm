c5cc89d5b1fa20e8feb53caf56c6115c77d99251
Néstor Andrés Peña <nestor2509@gmail.com>
2020-02-17 10:15:09 -0500

    update deploy

--- assets/bloques/bloque-lista-tarjetas.php
+++ assets/bloques/bloque-lista-tarjetas.php
@@ -30,7 +30,6 @@ $id = 'botones-' . $block['id'];
         </div>
         <div class="container_filtro tipos">
             <div class="item_filtro name active">Año</div>
-            <div class="item_filtro filter" data-filter="fecha_2018">2012</div>
             <div class="item_filtro filter" data-filter="fecha_2018">2013</div>
             <div class="item_filtro filter" data-filter="fecha_2018">2014</div>
             <div class="item_filtro filter" data-filter="fecha_2018">2015</div>
