3ed1c763585722a848478cd57a4f8017f1656d2c
Néstor Andrés Peña <nestor2509@gmail.com>
2020-02-18 12:43:14 -0500

    arreglo numero listado noticias

--- includes/campos/listado-noticias-auto.php
+++ includes/campos/listado-noticias-auto.php
@@ -54,7 +54,7 @@ if( function_exists('acf_add_local_field_group') ):
                 array(
                     'param' => 'block',
                     'operator' => '==',
-                    'value' => 'all',
+                    'value' => 'acf/listado-noticias-automatico',
                 ),
             ),
         ),
