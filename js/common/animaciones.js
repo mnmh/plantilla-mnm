e8682dc007bfb833a37c5c78f6ce7f35802fadb5
Néstor Andrés Peña <nestor2509@gmail.com>
2020-02-19 10:45:38 -0500

    animaciones slider columna

--- js/common/animaciones.js
+++ js/common/animaciones.js
@@ -72,7 +72,7 @@
 
             tl_temp
             .fromTo($(this).find('.left > *') ,.5, {autoAlpha: 0}, {autoAlpha: 1})
-            .fromTo($(this).find('.right .item') ,.5, {autoAlpha: 0, x: 20}, {autoAlpha: 1, x: 0})
+            .fromTo($(this).find('.right .item') ,.5, {autoAlpha: 0}, {autoAlpha: 1})
 
             var scene = new ScrollMagic.Scene({
                 triggerElement: this
