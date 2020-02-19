befe7e167e840c9119ed7d00e9a1df0936a6f964
Néstor Andrés Peña <nestor2509@gmail.com>
2020-02-19 10:47:25 -0500

    arreglos orden animaciones

--- js/common/animaciones.js
+++ js/common/animaciones.js
@@ -71,8 +71,8 @@
             var tl_temp = new TimelineMax();
 
             tl_temp
-            .fromTo($(this).find('.left > *') ,.5, {autoAlpha: 0}, {autoAlpha: 1})
             .fromTo($(this).find('.right .item') ,.5, {autoAlpha: 0}, {autoAlpha: 1})
+            .fromTo($(this).find('.left > *') ,.5, {autoAlpha: 0}, {autoAlpha: 1})
 
             var scene = new ScrollMagic.Scene({
                 triggerElement: this
