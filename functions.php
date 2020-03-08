997ec7837ecb6a16acf409c1164bc3b795bdd431
Néstor Andrés Peña <nestor2509@gmail.com>
2020-03-08 00:53:14 -0500

    defer parsing

--- functions.php
+++ functions.php
@@ -447,4 +447,11 @@ function html5_shortcode_demo_2($atts, $content = null) // Demo Heading H2 short
     return '<h2>' . $content . '</h2>';
 }
 
+function defer_parsing_of_js ( $url ) {
+    if ( FALSE === strpos( $url, '.js' ) ) return $url;
+    if ( strpos( $url, 'jquery.js' ) ) return $url;
+    return "$url' defer ";
+    }
+    add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 );
+
 ?>
