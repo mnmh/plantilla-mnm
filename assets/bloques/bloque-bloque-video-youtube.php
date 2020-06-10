957c0f5a3a44c6cfb3ba8077ca2b3ac6bbca3808
Nestor Andres Pena <nestor2509@gmail.com>
2020-06-10 12:52:32 -0500

    arreglo margenes bloique video

--- assets/bloques/bloque-bloque-video-youtube.php
+++ assets/bloques/bloque-bloque-video-youtube.php
@@ -10,6 +10,6 @@ $id = 'bloque-' . $block['id'];
 $videoid = get_field('videoid');
 ?>
 
-<div class="videoFrame halfbtm">
+<div class="videoFrame btm">
 <iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo $videoid ?>" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
