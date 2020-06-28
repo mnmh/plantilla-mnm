fb5983fceae515fca0392ed51dfcb939e4f044e7
Nestor Andres Pena <nestor2509@gmail.com>
2020-06-28 07:46:09 -0500

    nuevo bloque de arte y cultura

---
+++ assets/bloques/bloque-curaduria-arte-cutura.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Block Name: Curaduría arte y cultura
+ *
+ */
+
+// id unico para el bloque
+$id = 'bloque-' . $block['id'];
+?>
+<div class="curaduria-ac">
+    <div class="left">
+        <a class="item"></a>
+        <a class="item"></a>
+    </div>
+    <div class="right">
+
+    </div>
+</div>
