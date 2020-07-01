6de1984fa5c69d333baa302b8908fadd7754ae02
Nestor Andres Pena <nestor2509@gmail.com>
2020-07-01 17:31:44 -0500

    activar navegacion mapa

--- js/bloques/linea-tiempo.js
+++ js/bloques/linea-tiempo.js
@@ -1,6 +1,11 @@
 (function ($, root, undefined) {
 
     $(function () {
+        var current = 1;
+        var total = $('.linea-tiempo .nav .dot').length;
+
+        // console.log(current, total)
+
         $('.linea-tiempo .nav .dot').on('click', function () {
             $('.linea-tiempo .nav .dot.active').removeClass('active');
             $(this).addClass('active');
@@ -9,6 +14,20 @@
             $('.linea-tiempo .top .item.active').removeClass('active');
             $('.linea-tiempo .top .item:nth-child(' + index + ')').addClass('active');
         });
+
+        $('.linea-tiempo .nav .next').on('click', function() {
+            if(current < total) {
+                current++;
+                $('.linea-tiempo .nav .dot:nth-child('+current+')').click();
+            }
+        })
+
+        $('.linea-tiempo .nav .prev').on('click', function() {
+            if(current > 1) {
+                current--
+                $('.linea-tiempo .nav .dot:nth-child('+current+')').click();
+            }
+        })
 
         $('.linea-tiempo .nav .dot:first-child').click();
     });
